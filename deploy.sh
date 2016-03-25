bc12a78db48697e716ef3face0a8057f987e7015
tamaoki <tamaoki@feedtailor.jp>
2016-03-25 15:11:23 +0900

    デプロイスクリプトを整理

--- deploy.sh
+++ deploy.sh
@@ -2,23 +2,15 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-# Build the project.
-hugo # if using a theme, replace by `hugo -t <yourtheme>`
-
-# Go To Public folder
+hugo -v
 cd public
-# Add changes to git.
-git add -A
 
-# Commit changes.
+git add -A
 msg="rebuilding site `date \"+%Y-%m-%dT%H:%M:%S+09:00\"`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-# Push source and build repos.
 git push origin master
 
-# Come Back
 cd ..
